dbdcbfd46c4806c3f44fb613e1c8928883494047
tamer_maarouf <tamermaarouf@gmail.com>
2022-04-21 00:19:04 +0200

    create folder

---
+++ NPM/src/index.js
@@ -0,0 +1,2 @@
+console.log('hello prettier');
+console.log('jdsafasfsa');
---
+++ typescript-exercise/dist/utilities/numbers.js
@@ -0,0 +1,27 @@
+// multiply
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+// add
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+// divide
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+// subtract
+const subtract = (num1, num2) => {
+    return num1 - num2;
+};
+// square
+const square = (num) => {
+    return num * num;
+};
+export default {
+    multiply,
+    subtract,
+    divide,
+    sum,
+    square
+};
