057bb663d009259f829ad469e51b655331b982b3
tamer_maarouf <tamermaarouf@gmail.com>
2022-04-25 03:23:16 +0200

    finsih

--- Write_Logging_Middleware/src/utils/logger.ts
+++ Write_Logging_Middleware/src/utils/logger.ts
@@ -1,9 +1,9 @@
 import express from "express";
 
-const logger = (req: express.Request,res:  express.Response, next: () => void): void => {
-    let url = req.url;
-    console.log(`${url} was visited`);
-    next();
-  };
-  
-  export default logger;
+const logger = (req: express.Request, res: express.Response, next: () => void): void => {
+  let url = req.url;
+  console.log(`${url} was visited`);
+  next();
+};
+
+export default logger;
